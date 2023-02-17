65b0f16e9aa8979591dc73d31faaf4a37cc18b13
Tehssanice <tessychidubem@gmail.com>
2023-02-17 21:39:33 +0000

    6 print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - This program prints all single digit numbers of base 10 starting from 0
+ * main - This program prints all single digit numbers of base 10 from 0
  *
  * Return: 0
  */
