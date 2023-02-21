eda15b34ad69733bc2b51472babeb93a2f32b2fc
Tehssanice <tessychidubem@gmail.com>
2023-02-21 22:55:27 +0000

    abs

---
+++ 0x02-functions_nested_loops/abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - This program prints the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
