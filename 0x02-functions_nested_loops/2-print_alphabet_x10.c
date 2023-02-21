31a107c4f4e42d77ad2d0e7bc39b6a90e50f9c2c
Tehssanice <tessychidubem@gmail.com>
2023-02-21 21:40:08 +0000

    add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - this program adds two integers
+ * @num1: first integer
+ * @num2: second integer
+ * Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
+
