1a90192de937f42502f5af563095ee05dc2e926e
Tehssanice <tessychidubem@gmail.com>
2023-02-22 09:57:56 +0000

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
+
 	if (a > b && b > c)
 	{
 		largest = a;
@@ -24,6 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
