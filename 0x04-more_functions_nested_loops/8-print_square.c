48063848dce2ef04741fb7393c629b09cafda31b
Tehssanice <tessychidubem@gmail.com>
2023-02-23 22:00:17 +0000

    Prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,14 +10,14 @@
 
 void print_square(int size)
 {
-	int i, j;
+	int i, c;
 
 	if (size <= 0)
 		_putchar('\n');
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < (size); j++)
+		for (c = 0; c < (size); c++)
 		{
 			_putchar('#');
 		}
