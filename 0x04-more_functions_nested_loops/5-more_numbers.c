8d1c07b3bfc4ed4b3f73dbac2d3ff799a8517ef8
Tehssanice <tessychidubem@gmail.com>
2023-02-23 18:06:50 +0000

    Print numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - This prints numbers 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+
+int i, c;
+	for (i = 1; i <= 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+			_putchar('1');
+			_putchar(c % 10 + '0');
+		}
+			_putchar('\n');
+		}
+}
