ec9be03ae45d27651d212a6ea2d83849f4568f96
Tehssanice <tessychidubem@gmail.com>
2023-02-27 15:31:29 +0000

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Entry point
+ * description: function that takes a pointer
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *ip;
+	
+	ip = &n;
+	putchar(ip)
+		return (0)
+}
