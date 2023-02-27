f39502469a4c072867e9f46d707097e175bca23c
Tehssanice <tessychidubem@gmail.com>
2023-02-27 19:14:36 +0000

    String in reverse

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * description: prints every other character of a string
+ * *str: variable
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
