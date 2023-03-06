2b56a9d757306c270dd3d3545738934d897f1b9c
Tehssanice <tessychidubem@gmail.com>
2023-03-06 16:18:06 +0000

    length of a prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,30 @@
 #include "main.h"
 
 /**
+ * _strspn - Entry point
+ * description: gets the length of a prefix substring
+ * @s: variable
+ * @accept: variable
  *
+ * Return: b
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int b = 0, c, d;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] != 32)
+		{
+			for (d = 0; accept[d] != '\0'; d++)
+			{
+				if (s[d] == accept[d])
+					b++;
+			}
+		}
+		else
+			return (b);
+	}
+		return (b);
+}
