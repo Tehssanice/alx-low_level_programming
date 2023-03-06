24fe3b2a63ef297eae49036a887860b63dc06b09
Tehssanice <tessychidubem@gmail.com>
2023-03-06 16:23:31 +0000

    searches a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * description: searches a string for a set of bytes
+ * @accept: variable
+ * @s: variable
+ *
+ * Return: s or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+		while (*s)
+		{
+			for (i = 0; accept[i]; i++)
+			{
+				if (*s == accept[i])
+					return (s);
+			}
+			s++;
+		}
+	return (NULL);
+}
