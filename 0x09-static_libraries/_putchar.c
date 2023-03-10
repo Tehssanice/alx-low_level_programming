42ad86ccbc51513d6e6fa3beb98c3f1be8051f23
Tehssanice <tessychidubem@gmail.com>
2023-03-10 18:00:07 +0000

    Update

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,9 +2,11 @@
 
 /**
  * _putchar - Entry point
- * description: prints
+ * description: writes the character c to stdout
+ * @c: The character to print
  *
- * Return: 0
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int _putchar(char c)
