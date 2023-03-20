82e2f20459a777a6f4f70a41f2b735d5308b3c4b
Tehssanice <tessychidubem@gmail.com>
2023-03-20 17:15:54 +0000

    Made updates on betty

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,7 +5,7 @@
  * file: 3-function_like_macro.h
  *
  * description: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
+ *	that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -4,7 +4,7 @@
 /*
  * file: 4-sum.h
  * description: Header file that defines a function-like
- * 	macro SUM(x, y) that computes the sum of the numbers x and y.
+ *	macro SUM(x, y) that computes the sum of the numbers x and y.
  */
 
 #define SUM(x, y) ((x) + (y))
