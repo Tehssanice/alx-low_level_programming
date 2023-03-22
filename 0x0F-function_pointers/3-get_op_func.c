dbddb156f19439fdb0371880c07acdb8c1f94f7c
Theresa Nwaru <100996643+Tehssanice@users.noreply.github.com>
2023-03-22 16:06:22 -0700

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,15 +18,14 @@ int (*get_op_func(char *s))(int, int)
 		{ "/", op_div },
 		{ "%", op_mod },
 		{ NULL, NULL }
-	};
+	int i = 0;
 
-	int i;
-
-	for (i = 0; i < 5; i++)
+	while (i < 5)
 	{
-
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+
+		i++;
 	}
 
 	return (0);
