5d36ffdbd3d219f1dfc8de9782d1257df5199f02
Tehssanice <tessychidubem@gmail.com>
2023-04-10 15:41:38 +0000

    changed file name

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number = 0;
-	int length = 0;
+	unsigned int ch = 0;
+	int len = 0;
 
-	if (b[length] == '\0')
+	if (b[len] == '\0')
 		return (0);
 
-	while ((b[length] == '0') || (b[length] == '1'))
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-		number <<= 1;
-		number += b[length] - '0';
-		length++;
+		ch <<= 1;
+		ch += b[len] - '0';
+		len++;
 	}
 
-	return (number);
+	return (ch);
 }
